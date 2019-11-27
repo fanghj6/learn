16e5d3a8067ee1f4b294a7a80aaaddcd57f01bbd
fanghj <fanghj6@163.com>
2019-11-27 10:10:32 +0800

    test mapstruct

--- junittest/src/main/java/com/junit/junittest/JunittestApplication.java
+++ junittest/src/main/java/com/junit/junittest/JunittestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.junit.junittest.mapper")
+@MapperScan({"com.junit.junittest.mapper"})
 public class JunittestApplication {
 
     public static void main(String[] args) {
--- junittest/src/main/java/com/junit/junittest/controller/UserController.java
+++ junittest/src/main/java/com/junit/junittest/controller/UserController.java
@@ -3,6 +3,8 @@ package com.junit.junittest.controller;
 import com.alibaba.fastjson.JSON;
 import com.junit.junittest.db.User;
 import com.junit.junittest.service.UserService;
+import com.junit.junittest.test.mapstruct.UserConverter;
+import com.junit.junittest.test.mapstruct.UserDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -36,5 +38,18 @@ public class UserController {
         User user = userService.selectByPrimaryKey(id);
         log.info("user is {}", JSON.toJSON(user));
         return user;
+    }
+
+    @Autowired
+    private UserConverter userConverter;
+
+    @GetMapping("/test/converter")
+    @ApiOperation(value = "selectByPrimaryKey", notes = "根据编号获取用户信息")
+    public UserDto userConverter(int id,String name) {
+        com.junit.junittest.test.mapstruct.User user = new com.junit.junittest.test.mapstruct.User();
+        user.setId(id);
+        user.setName(name);
+        UserDto dto = userConverter.DoToDto(user);
+        return dto;
     }
 }
