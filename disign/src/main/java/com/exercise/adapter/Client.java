72594824bd7e013aa1a9bb55ad24fe71704b5baf
fanghj <fanghj6@163.com>
2019-12-16 14:59:28 +0800

    test cloneable

--- disign/src/main/java/com/exercise/adapter/Client.java
+++ disign/src/main/java/com/exercise/adapter/Client.java
@@ -1,5 +1,6 @@
 package com.exercise.adapter;
 
+import java.io.FileNotFoundException;
 /**
  * @author fhj
  * @version 1.0
@@ -9,7 +10,7 @@ package com.exercise.adapter;
  **/
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Operation operation = new Operation();
         Target target = new Adapter(operation);
         Double result = target.request("100","200");
