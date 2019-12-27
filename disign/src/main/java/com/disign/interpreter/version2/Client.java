f06fb7440a85d025d3c0dc5e238475c2e3585df5
fanghj <fanghj6@163.com>
2019-12-27 15:40:55 +0800

    test behavior version1

--- disign/src/main/java/com/disign/interpreter/version2/Client.java
+++ disign/src/main/java/com/disign/interpreter/version2/Client.java
@@ -10,6 +10,12 @@ package com.disign.interpreter.version2;
 public class Client {
 
     public static void main(String[] args) {
+//        String text="LOOP 2 PRINT 杨过 SPACE SPACE PRINT 小龙女 BREAK END PRINT 郭靖 SPACE SPACE PRINT 黄蓉 BREAK";
+//        Context context=new Context(text);
+//        Node node = new ExpressionNode();
+//        node.interpret(context);
+//        node.execute();
+
         String text="LOOP 2 PRINT 杨过 SPACE SPACE PRINT 小龙女 BREAK END PRINT 郭靖 SPACE SPACE PRINT 黄蓉 BREAK";
         Context context=new Context(text);
         Node node = new ExpressionNode();
